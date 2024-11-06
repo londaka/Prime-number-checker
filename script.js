0dd2f2746b86d043c52cdcb37a07bbc62f9cb101
londaka <sanjayapokhrel41@gmail.com>
2024-11-06 15:23:58 +0545

    Add script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+function checkPrime(num){
+    if(num <= 1){
+        return false;
+    }
+    for(let i=2; i <= Math.sqrt(num); i++){
+        if(num % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+//implementing DOM element for html file
+
+document.getElementById("sub-id"),addEventListener("click", function(){
+    let num = document.getElementById("input-id").value;
+    let result = checkPrime(num);
+
+    if(result){
+        document.getElementById("dom-msg").innerHTML = num + "is prime number.";
+    }else{
+        this.document.getElementById("dom-msg").innerHTML = num + "is not a prime number.";
+    }
+
+});
+
+document.getElementById("id-clear").addEventListener("click", () =>{
+    document.getElementById("input-id").value = "";
+    document.getElementById("dom-msg").innerHTML = "Resutl";
+})
